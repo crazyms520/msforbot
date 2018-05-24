ee2ff4b400db9cf9971dd002352ad57c691171ee
hank_chang <hank_chang@friday.tw>
2018-05-24 14:51:12 +0800

    console message

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function handleEvent(event) {
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text }
-  console.log('res:'+echo);
+  console.log('res:'+event.message.text);
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
