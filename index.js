1d952cc955d22eccc1f17b8ba218d7129dee7ab9
hank_chang <hank_chang@friday.tw>
2018-05-24 17:45:48 +0800

    test

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function handleEvent(event) {
 
   // create a echoing text message
   const echo = { type: 'text', text: event.message.text }
-  console.log('res:'+event.message);
+  console.log('res:'+JSON.stringify(event.message));
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
