8322dbb943ce15d0d5b254f6bdd466405a07c277
張欽揚 <crazyms520@hotmail.com>
2018-06-06 14:15:25 +0800

    get User fix2

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
   var user = client.getProfile (event.source.userId);
+  console.log ('this user');
   console.log (user);
 //   // create a echoing text message
   const echo = { type: 'text', text: event.message.text }
