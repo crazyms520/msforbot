8c1046e2d318418f30e0ef81a9059e173939b701
張欽揚 <crazyms520@hotmail.com>
2018-06-06 13:57:40 +0800

    test line bot3

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ function handleEvent(event) {
   }
 
 //   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text }
-  console.log('res1: ' + JSON.stringify(event));
-  console.log('res2: ' + JSON.stringify(event.message));
+  const echo = { type: 'text', text: event.message.text, userId: event.source.userId }
 
 //   // use reply API
   return client.replyMessage(event.replyToken, echo);
