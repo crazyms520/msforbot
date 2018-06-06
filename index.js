a2b8281b85f1f8c70dab7672cda2ed839f42b3a5
張欽揚 <crazyms520@hotmail.com>
2018-06-06 11:17:00 +0800

    test find by query2

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const findDocuments = function(db, callback) {
   // Get the documents collection
   const collection = db.collection('test');
   // Find some documents
-  collection.find({"test":"1"}).toArray(function(err, docs) {
+  collection.find({test:1}).toArray(function(err, docs) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs)
