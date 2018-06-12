2ac1bdab05194038a520e6298b2d31e14559e48d
張欽揚 <crazyms520@hotmail.com>
2018-06-12 14:44:20 +0800

    add crawler console

--- modules/crawler.js
+++ modules/crawler.js
@@ -3,22 +3,20 @@ const cheerio = require('cheerio');
 
 const appleCrawler = function () {
     const url = 'https://tw.appledaily.com/search'
-    
+    let data = [];
     let results = request.post({
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url:     url,
         body:    "querystrS=食安&searchType=text&searchMode=Sim"
     }, function(error, response, body){
         let $ = cheerio.load(body);
-        results = [];
         $('.tbb > h2').each(function(i, elem) {
             // results.push('蘋果')
-            results.push($(this).text())
-            results.push($('.tbb > h2 > a').attr('href'))
+            data.push($(this).text())
+            data.push($('.tbb > h2 > a').attr('href'))
         })
-        return results;
   });
-  return results;
+  return data;
 }
 
 module.exports = appleCrawler;
