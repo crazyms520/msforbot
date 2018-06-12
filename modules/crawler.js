016e56f58de18e089b42a82f4bebeb0f7e03a453
張欽揚 <crazyms520@hotmail.com>
2018-06-12 14:38:04 +0800

    add crawler return

--- modules/crawler.js
+++ modules/crawler.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const appleCrawler = function () {
     const url = 'https://tw.appledaily.com/search'
     
-    request.post({
+    let results = request.post({
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url:     url,
         body:    "querystrS=食安&searchType=text&searchMode=Sim"
@@ -16,6 +16,7 @@ const appleCrawler = function () {
             results.push($(this).text())
             results.push($('.tbb > h2 > a').attr('href'))
         })
+        return results;
   });
   return results;
 }
