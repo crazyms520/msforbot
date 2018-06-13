c98c23e72f62a44efeadfafb39a5ce590ee20bf8
張欽揚 <crazyms520@hotmail.com>
2018-06-13 14:03:10 +0800

    test button template

--- index.js
+++ index.js
@@ -54,41 +54,41 @@ function handleEvent(event) {
       // create a echoing text message
       // const echo = { type: 'text', text: profile.displayName+' say : '+ apple }
       // use reply API
-      const echo = {
-        "type": "template",
-        "altText": "This is a buttons template",
-        "template": {
-          "type": "buttons",
-          "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-          "imageAspectRatio": "rectangle",
-          "imageSize": "cover",
-          "imageBackgroundColor": "#FFFFFF",
-          "title": "Menu",
-          "text": "Please select",
-          "defaultAction": {
-              "type": "uri",
-              "label": "View detail",
-              "uri": "http://example.com/page/123"
-          },
-          "actions": [
-              {
-                "type": "postback",
-                "label": "Buy",
-                "data": "action=buy&itemid=123"
-              },
-              {
-                "type": "postback",
-                "label": "Add to cart",
-                "data": "action=add&itemid=123"
-              },
-              {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://example.com/page/123"
-              }
-          ]
-        }
-      }
+      // const echo = {
+      //   "type": "template",
+      //   "altText": "This is a buttons template",
+      //   "template": {
+      //     "type": "buttons",
+      //     "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
+      //     "imageAspectRatio": "rectangle",
+      //     "imageSize": "cover",
+      //     "imageBackgroundColor": "#FFFFFF",
+      //     "title": "Menu",
+      //     "text": "Please select",
+      //     "defaultAction": {
+      //         "type": "uri",
+      //         "label": "View detail",
+      //         "uri": "http://example.com/page/123"
+      //     },
+      //     "actions": [
+      //         {
+      //           "type": "postback",
+      //           "label": "Buy",
+      //           "data": "action=buy&itemid=123"
+      //         },
+      //         {
+      //           "type": "postback",
+      //           "label": "Add to cart",
+      //           "data": "action=add&itemid=123"
+      //         },
+      //         {
+      //           "type": "uri",
+      //           "label": "View detail",
+      //           "uri": "http://example.com/page/123"
+      //         }
+      //     ]
+      //   }
+      // }
       return client.replyMessage(event.replyToken, echo);
     });
   } else {
