d325f12ffb865c70ca13e0dafdbea4b012dbabcf
張欽揚 <crazyms520@hotmail.com>
2018-06-14 15:02:58 +0800

    crawler test

--- index.js
+++ index.js
@@ -114,8 +114,7 @@ function handleEvent(event) {
               type: 'text',
               text: '111'
             }
-            client.replyMessage(event.replyToken, test2);
-            return client.replyMessage(event.replyToken, test2);
+            return client.replyMessage(event.replyToken, test2, test2);
           })
           .catch((err) => {
             console.error('err:' + err);
