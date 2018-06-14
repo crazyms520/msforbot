c3588cbf83d3eced216a9c1ad1e472c534e59c3d
張欽揚 <crazyms520@hotmail.com>
2018-06-14 15:29:56 +0800

    crawler test

--- index.js
+++ index.js
@@ -97,9 +97,9 @@ function handleEvent(event) {
         Promise
           .all(queryStr.map(appleCrawler))
           .then((result) => {
-            const data1 = result[0].concat(result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],result[10]);
+            let data1 = result[0].concat(result[1],result[2],result[3],result[4],result[5],result[6],result[7],result[8],result[9],result[10]);
             data1 = data1.join('\n')
-            const data2 = result[11].concat(result[12],result[13],result[14],result[15],result[16],result[17],result[18]);
+            let data2 = result[11].concat(result[12],result[13],result[14],result[15],result[16],result[17],result[18]);
             data2 = data2.join('\n')
             const echo = [{
               type: 'text',
