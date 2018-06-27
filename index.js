930c147c20cef80984789c2aa9c127546718b4af
張欽揚 <crazyms520@hotmail.com>
2018-06-27 15:18:21 +0800

    game test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const line        = require('@line/bot-sdk');
 const express     = require('express');
-const gameCrawler = require('crawler');
+const gameCrawler = require('gameCrawler');
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
