a5abdb36981f8b055ca3b6a6bbc67a990e33beff
張欽揚 <crazyms520@hotmail.com>
2018-06-27 23:00:35 +0800

    game test

--- modules/gameCrawler.js
+++ modules/gameCrawler.js
@@ -7,8 +7,9 @@ function crawler(url) {
         request.get(url, function (error, response, body) {
             let $ = cheerio.load(body);
             $('.b-list__row').each(function(i,elem) {
+                console.log($(this).children('.b-list__main').children('a').text().includes('本討論串已無文章'));
                 if ($(this).children('.b-list__time').text().includes('今日') && (!$(this).children('.b-list__main').children('a').text().includes('本討論串已無文章'))) {
-                 result.push ($(this).children('.b-list__main').children('a').text() + ' \n ' + 'https://forum.gamer.com.tw/' + $(this).children('.b-list__main').children('a').attr('href') + ' \n ');   
+                   result.push ($(this).children('.b-list__main').children('a').text() + ' \n ' + 'https://forum.gamer.com.tw/' + $(this).children('.b-list__main').children('a').attr('href') + ' \n ');   
                 }
                 
             });
