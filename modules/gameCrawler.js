4c331c93d7ae56419a72533ad966905ee7643b31
張欽揚 <crazyms520@hotmail.com>
2018-06-27 23:11:09 +0800

    game test

--- modules/gameCrawler.js
+++ modules/gameCrawler.js
@@ -8,7 +8,7 @@ function crawler(url) {
             let $ = cheerio.load(body);
             $('.b-list__row').each(function(i,elem) {
                 console.log($(this).children('.b-list__main').children('a').text().includes('本討論串已無文章'));
-                if ($(this).children('.b-list__time').text().includes('今日') && ($(this).children('.b-list__main').children('.b-list__main__title is-del').length === 0)) {
+                if ($(this).children('.b-list__time').text().includes('今日') && ($(this).children('.b-list__main').children('.b-list__main__title.is-del').length === 0)) {
                    result.push ($(this).children('.b-list__main').children('a').text() + ' \n ' + 'https://forum.gamer.com.tw/' + $(this).children('.b-list__main').children('a').attr('href') + ' \n ');   
                 }
                 
